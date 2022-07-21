07867b09d1ebd13f11ea3733634d3305a6ffedd3
Haira Lei <darhaii13@gmail.com>
2022-07-21 22:50:08 +0800

    Add notifications feature and finish project

--- src/App.js
+++ src/App.js
@@ -4,21 +4,48 @@ import Form from "./components/Form";
 import List from "./components/List";
 
 function App() {
+  //Load data from local storage
   const localData =
     JSON.parse(window.localStorage.getItem("groceryList")) || [];
+
+  //States
   const [groceryList, setGroceryList] = useState(localData);
   const [item, setItem] = useState("");
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState("");
-  const [error, setError] = useState(false);
+  const [notif, setNotif] = useState("");
+  const [notifType, setNotifType] = useState("success");
+  //
+
+  //Functions
 
   const handleChangeInput = (e) => {
     setItem(e.target.value);
   };
 
+  //For notification if success or error
+  const checkValidation = (item) => {
+    if (item.trim().length === 0) {
+      setNotifType("error");
+      setNotif("Please enter valid data");
+      return;
+    } else {
+      setNotifType("success");
+      setNotif(isEditing ? "Item updated" : "Item added to the list");
+    }
+  };
+
+  //Clear notif after 3seconds
+  useEffect(() => {
+    const timeout = setTimeout(() => setNotif(""), 3000);
+
+    return () => clearInterval(timeout);
+  }, [notif]);
+
   const handleAddItem = (e) => {
     e.preventDefault();
 
+    checkValidation(item);
     if (item.trim().length === 0) return;
 
     const randomId =
@@ -58,15 +85,33 @@ function App() {
 
       return [...newList];
     });
+
+    setNotifType("error");
+    setNotif("Item deleted from the list");
   };
 
+  //Save to local storage
   useEffect(() => {
     window.localStorage.setItem("groceryList", JSON.stringify(groceryList));
   }, [groceryList]);
 
+  //
+
   return (
     <main className="App">
-      <div className="error"></div>
+      {notif && (
+        <div
+          className="notif"
+          style={{
+            backgroundColor: `${
+              notifType === "success" ? "#008BFF" : "#ED195A"
+            }`,
+          }}
+        >
+          {notif}
+        </div>
+      )}
+
       <h1 className="primary-header">Grocery List</h1>
       <Card>
         <Form
